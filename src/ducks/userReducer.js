51a6dc51e1826426c3869181ae4ab568ff1c7a13
Alex Crowe <alexcwolf5@gmail.com>
2021-01-21 22:01:12 -0700

    working

---
+++ src/ducks/userReducer.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+//INITIAL STATE
+
+const initialState = {
+    email: null,
+    firstName: null,
+    lastName: null
+}
+
+//ACTION CONSTANTS
+const REQUEST_USER_DATA = 'REQUEST_USER_DATA'
+
+
+//ACTION CREATOR
+export const requestUserData = () => {
+    let data = axios.get('/auth/user-data').then(res => res.data)
+    return {
+        type: REQUEST_USER_DATA,
+        payload: data
+    }
+}
+
+
+//THE REDUCER
+export default function reducer(state = initialState, action) {
+    switch (action.type) {
+        case REQUEST_USER_DATA + '_FUFILLED':
+            const { email, firstName, lastName } = action.payload.user
+            return { email, firstName, lastName }
+        default:
+            return state
+    }
+}
